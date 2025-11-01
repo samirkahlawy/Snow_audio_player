f3f9fe1f6fd09c2b7b9262e974bb2b4c7346fa18
Samir Kahlawy <195639151+samirkahlawy@users.noreply.github.com>
2025-11-01 14:21:36 +0200

    Add PlaylistComponent class for audio track management

---
+++ PlaylistComponent.h
@@ -0,0 +1,40 @@
+/*
+  ==============================================================================
+
+    PlaylistComponent.h
+    Created: 1 Nov 2025 9:54:10am
+    Author:  ME
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+#include "PlayerAudio.h"
+
+class PlaylistComponent : public juce::Component,
+    public juce::TableListBoxModel
+{
+public:
+    PlaylistComponent(PlayerAudio& player);
+    ~PlaylistComponent() override = default;
+
+    int getNumRows() override;
+
+    void paintRowBackground(juce::Graphics& g, int rowNumber, int width, int height, bool rowIsSelected) override;
+
+    void paintCell(juce::Graphics& g, int rowNumber, int columnId, int width, int height, bool rowIsSelected) override;
+
+    void loadFiles();
+
+    void playSelectedTrack();
+
+    void resized() override;
+
+private:
+    PlayerAudio& audioPlayer;
+    juce::TableListBox table;
+    juce::Array<juce::File> trackFiles;
+    juce::FileChooser chooser{ "Select multiple audio files..." };
+
+};
